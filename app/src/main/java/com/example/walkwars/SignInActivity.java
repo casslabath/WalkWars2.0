81e22ed930730955052a2eef0a707be831c5c1e5
Cassie <casslabath@yahoo.com>
2020-05-27 18:43:43 -0500

    runs signin activity through main

--- app/src/main/java/com/example/walkwars/SignInActivity.java
+++ app/src/main/java/com/example/walkwars/SignInActivity.java
@@ -1,4 +1,14 @@
 package com.example.walkwars;
 
-public class SignInActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
 }
